f9c7aeb45d1e2bc9d7fc70a922192eaf29e8c74b
Adil Tehranli <adiljt@code.edu.az>
2023-07-24 19:43:37 +0400

    Updated datatable

--- AdoNetCustomData/Controllers/HomeController.cs
+++ AdoNetCustomData/Controllers/HomeController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdoNetCustomData.Helpers;
+using AdoNetCustomData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace AdoNetCustomData.Controllers
 {
     public class HomeController:Controller
     {
-        public IActionResult Index()
-        { 
-            return View();
-        } 
+        public async Task<IActionResult>  Index()
+        {
+            string connstr = "Server=MSI\\SQLEXPRESS;Database=ObaMarket;Trusted_Connection=True;Encrypt=false";
+            List<Branch> branches = new List<Branch>();
+          SqlNetHelper sqlHelper = new SqlNetHelper();
+            DataTable dt = await  sqlHelper.SelectAsync("SELECT * FROM Branch");
+                    foreach ( DataRow item in dt.Rows)
+                    {
+                        branches.Add(new()
+                        {
+                            Id = (int)item[0],
+                            Name =(string)item[1]
+                        });
+                    }
+            return View(branches);
+                
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Branch(string name)
+        {
+            string connstr = "Server=MSI\\SQLEXPRESS;Database=ObaMarket;Trusted_Connection=True;Encrypt=false";
+            List<Branch> branches = new List<Branch>();
+            using SqlConnection conn = new SqlConnection(connstr);
+            
+                await conn.OpenAsync();
+            using SqlCommand command = new SqlCommand($"INSERT INTO Branch VALUES ('{name}')", conn);
+                
+                 int res =  await command.ExecuteNonQueryAsync();
+                
+            return Content(name);
+        }
     }
 }
---
+++ AdoNetCustomData/Helpers/SqlNetHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AdoNetCustomData.Helpers
+{
+    public class SqlNetHelper
+    {
+        private string _connectionStr = "Server=MSI\\SQLEXPRESS;Database=ObaMarket;Trusted_Connection=True;Encrypt=false";
+
+		public string ConnectionStr 
+		{
+			get { return _connectionStr ; }
+			
+		}
+
+		public async Task<DataTable>  SelectAsync(string query)
+		{
+			using(SqlConnection conn  = new SqlConnection(ConnectionStr))
+			{
+				await conn.OpenAsync();
+				using (SqlDataAdapter sda = new SqlDataAdapter(query,conn))
+				{
+					DataTable dt = new DataTable();
+					sda.Fill(dt);
+					return dt;
+				}
+			}
+		}
+	}
+}
