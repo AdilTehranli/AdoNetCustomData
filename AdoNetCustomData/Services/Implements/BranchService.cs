e8e32dedb004ad70bb152eb0e48e8bc8378f9370
Adil Tehranli <adiljt@code.edu.az>
2023-07-24 21:04:22 +0400

    Updated getbyid

---
+++ AdoNetCustomData/Services/Implements/BranchService.cs
@@ -0,0 +1,45 @@
+﻿using AdoNetCustomData.Helpers;
+using AdoNetCustomData.Models;
+using AdoNetCustomData.Services.Interfaces;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace AdoNetCustomData.Services.Implements;
+
+public class BranchService : IBranchService
+{
+    public async Task<int> AddAsync(Branch branch)
+    {
+      return await  SqlNetHelper.ExecuteAsync($"INSERT INTO Branch VALUES ('{branch.Name}')");
+    }
+
+    public async Task<int> AddAsync(List<Branch> branch)
+    {
+        string query = "INSERT INTO Branch VALUES";
+        foreach (Branch branch1 in branch)
+        {
+            query += $"(N'{branch1.Name})";
+        }
+        return await SqlNetHelper.ExecuteAsync(query.Substring(0,query.Length-1));
+    }
+
+    public int Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Branch>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Branch> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Branch Update(int id, Branch branch)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AdoNetCustomData/Services/Interfaces/IBranchService.cs
@@ -0,0 +1,14 @@
+﻿using AdoNetCustomData.Models;
+
+namespace AdoNetCustomData.Services.Interfaces;
+
+public interface IBranchService
+{
+    public Task<int> AddAsync(Branch branch);
+    public Task<int> AddAsync(List<Branch> branch);
+    public Task<List<Branch>> GetAllAsync();
+    public Task<Branch> GetByIdAsync(int id);
+    public int Delete(int id);
+    public Branch Update(int id,Branch branch);
+}
+
