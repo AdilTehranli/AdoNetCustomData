ed707587f70babd80648a644f5e9ab49ea2abdf0
Adil Tehranli <adiljt@code.edu.az>
2023-07-24 23:20:13 +0400

    Updated service

--- AdoNetCustomData/Controllers/HomeController.cs
+++ AdoNetCustomData/Controllers/HomeController.cs
@@ -8,35 +8,42 @@ using System.Data;
 
 namespace AdoNetCustomData.Controllers
 {
-    public class HomeController:Controller
+    public class HomeController : Controller
     {
-        public async Task<IActionResult>  Index()
+        public async Task<IActionResult> Index()
         {
             List<Branch> branches = new List<Branch>();
-            DataTable dt = await  SqlNetHelper.SelectAsync("SELECT * FROM Branch");
-                    foreach ( DataRow item in dt.Rows)
-                    {
-                        branches.Add(new()
-                        {
-                            Id = (int)item[0],
-                            Name =(string)item[1]
-                        });
-                    }
+            DataTable dt = await SqlNetHelper.SelectAsync("SELECT * FROM Branch");
+            foreach (DataRow item in dt.Rows)
+            {
+                branches.Add(new()
+                {
+                    Id = (int)item[0],
+                    Name = (string)item[1]
+                });
+            }
             return View(branches);
-                
+
 
         }
         [HttpPost]
         public async Task<IActionResult> Branch(string name)
         {
-          var res=  await SqlNetHelper.ExecuteAsync($"INSERT INTO Branch VALUES ('{name}')");
+            var res = await SqlNetHelper.ExecuteAsync($"INSERT INTO Branch VALUES ('{name}')");
             return Content(res.ToString());
         }
         public async Task<IActionResult> BranchGetAll()
         {
-           IBranchService service = new BranchService();
+            IBranchService service = new BranchService();
             return Json(await service.GetAllAsync());
         }
+        public async Task<IAsyncResult> BranchGetById(int id)
+        {
+            IBranchService service = new BranchService();
+            return (IAsyncResult)Json(await service.GetByIdAsync(id));
+        }
+      
+
 
     }
 }
